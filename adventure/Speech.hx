b87b0f3734e29eaa7f85dad21db0e28243f185f0
dixonary <alex@dixonary.co.uk>
2016-11-18 23:40:19 +0000

    initial

--- adventure/Speech.hx
+++ adventure/Speech.hx
@@ -3,7 +3,6 @@ package adventure;
 import flixel.group.FlxSpriteGroup;
 import flixel.text.FlxText;
 import flixel.FlxG;
-using Definitions;
 
 class Speech extends FlxSpriteGroup {
     public var text:FlxText;
