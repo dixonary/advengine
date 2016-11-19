301b156900a5b1aa059d1cda5dc3a2b7e4752e7c
dixonary <alex@dixonary.co.uk>
2016-11-19 17:52:54 +0000

    initial

--- adventure/Game.hx
+++ adventure/Game.hx
@@ -146,7 +146,6 @@ class Game extends FlxState {
 
         if(Global.rooms.get(rname) == null) {
             Global.rooms.set(rname, R.createInstance([]));
-            trace(Global.rooms);
             Global.currentRoom = Global.rooms.get(rname);
             Global.currentRoom.create();
         }
