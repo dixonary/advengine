3a0c62ad7d39751faa2f56c3c755250d8a923531
dixonary <alex@dixonary.co.uk>
2018-02-13 11:46:39 +0000

    object ticks

--- adventure/Tick.hx
+++ adventure/Tick.hx
@@ -5,14 +5,17 @@ import flixel.FlxG;
 import flixel.math.FlxPoint;
 import flixel.text.FlxText;
 using flixel.util.FlxSpriteUtil;
+using flixel.tweens.FlxTween;
 
 class Tick extends FlxText {
 
-    var TICK_SIZE:Int = 64;
     var TICK_OFFSET:Int = 64;
     static var ticks:Array<Tick> = [];
 
     var callback:Void->Void;
+
+    var floatTime:Float = 0;
+    var allowed:Bool = false;
 
     public function new(X:Float,Y:Float,Angle:Float,Word:String, Callback:Void->Void) {
         super();
@@ -26,7 +29,7 @@ class Tick extends FlxText {
         y = Y;
 
 
-        x += Math.sin(Angle) * TICK_OFFSET - TICK_SIZE/2;
+        x += Math.sin(Angle) * TICK_OFFSET - TICK_OFFSET/2;
         y -= Math.cos(Angle) * TICK_OFFSET;
 
         callback = Callback;
@@ -34,6 +37,15 @@ class Tick extends FlxText {
         setBorderStyle(OUTLINE,0xff000000,2);
 
         ticks.push(this);
+
+        scale.set(0.1,0.1);
+        scale.tween({x:1}, 0.2, {complete:function(t) {
+            scale.tween({y:1}, 0.2, {complete:function(t) {
+                allowed = true;
+            }});
+        }});
+
+        floatTime += Math.random()*Math.PI*2;
 
     }
 
@@ -49,6 +61,13 @@ class Tick extends FlxText {
     override public function update(d) {
         super.update(d);
 
+        offset.y -= Math.sin(floatTime)*room.scaleFactor/2;
+        floatTime += d*1.25;
+        floatTime %= Math.PI*2;
+        offset.y += Math.sin(floatTime)*room.scaleFactor/2;
+
+        if(!allowed) return;
+
         if(
             Math.pow(FlxG.mouse.x-x-width/2,2) +
             Math.pow(FlxG.mouse.y-y-height/2,2) <
@@ -60,6 +79,7 @@ class Tick extends FlxText {
             }
         }
         else color = 0xffffffff;
+
     }
 
     function hit() {
